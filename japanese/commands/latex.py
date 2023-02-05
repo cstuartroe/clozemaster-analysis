22207ba4a4a4c2576193331d3009dc2be4e9c1e7
Conor Stuart-Roe <conorstuartroe@gmail.com>
2023-02-04 21:25:06 -0500

    LaTeX excapes

--- japanese/commands/latex.py
+++ japanese/commands/latex.py
@@ -26,23 +26,30 @@ LATEX_TEMPLATE = r"""
 CJK_FONT = "min"
 
 
+def latex_escape(s: str, cjk_permitted: bool):
+    s = s.replace('$', '\\$').replace('&', '\\&').replace('%', '\\%')
+    if not cjk_permitted:
+        s = s.replace('・', '·')
+    return s
+
+
 @with_words
 @command
 def latex(el: JapaneseExerciseList, namespace: argparse.Namespace):
     most_common_kanji = sorted(
         el.character_counts(namespace.words)[CharacterType.KANJI].items(),
-        key=lambda x: x[1],
+        key=lambda x: len(x[1]),
         reverse=True,
     )
 
     content = ""
 
-    for character, count in most_common_kanji:
+    for character, all_exercises in most_common_kanji:
         content += (
             f"\\begin{{CJK}}{{UTF8}}{{{CJK_FONT}}}"
             f"\\section{{{character}}}"
             "\\end{CJK}\n\n"
-            f"{count} occurrences\n\n"
+            f"{len(all_exercises)} occurrences\n\n"
             "\\bigskip\n\n"
         )
 
@@ -59,9 +66,9 @@ def latex(el: JapaneseExerciseList, namespace: argparse.Namespace):
 
             content += (
                 f"\\begin{{CJK}}{{UTF8}}{{{CJK_FONT}}}"
-                f"{exercise.pronunciation}"
+                f"{latex_escape(exercise.pronunciation, cjk_permitted=True)}"
                 "\\end{CJK}\n\n"
-                f"{exercise.translation}\n\n"
+                f"{latex_escape(exercise.translation, cjk_permitted=False)}\n\n"
                 "\\bigskip\n\n"
             )
 
